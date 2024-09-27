9d3fede1e532dd6f6d17ea26c277829e31fef0ba
dlegland <david.legland@inrae.fr>
2024-09-27 17:01:57 +0200

    add Centroid feature

---
+++ src/main/java/net/ijt/regfeat/morpho2d/Centroid.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package net.ijt.regfeat.morpho2d;
+
+import java.awt.geom.Point2D;
+
+import ij.measure.Calibration;
+import ij.measure.ResultsTable;
+import ij.process.ImageProcessor;
+import net.ijt.regfeat.Feature;
+import net.ijt.regfeat.RegionAnalyisData;
+
+/**
+ * Computes centroid of each regions within the label map.
+ */
+public class Centroid extends Feature
+{
+    @Override
+    public Point2D[] compute(RegionAnalyisData results)
+    {
+        ImageProcessor labelMap = results.labelMap.getProcessor();
+        Calibration calib = results.labelMap.getCalibration();
+        return inra.ijpb.measure.region2d.Centroid.centroids(labelMap, results.labels, calib);
+    }
+    
+    @Override
+    public void populateTable(ResultsTable table, Object obj)
+    {
+        if (obj instanceof Point2D[])
+        {
+            Point2D[] array = (Point2D[]) obj;
+            for (int r = 0; r < array.length; r++)
+            {
+                Point2D point = array[r];
+                table.setValue("Centroid.X", r, point.getX());
+                table.setValue("Centroid.Y", r, point.getY());
+            }
+        }
+        else
+        {
+            throw new RuntimeException("Requires object argument to be an array of Point2D");
+        }
+    }
+}
---
+++ src/test/java/net/ijt/regfeat/morpho2d/CentroidTest.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package net.ijt.regfeat.morpho2d;
+
+import static org.junit.Assert.*;
+
+import java.awt.geom.Point2D;
+
+import org.junit.Test;
+
+import ij.ImagePlus;
+import ij.measure.ResultsTable;
+import ij.process.ByteProcessor;
+import ij.process.ImageProcessor;
+import inra.ijpb.label.LabelImages;
+import net.ijt.regfeat.RegionAnalyisData;
+
+/**
+ * 
+ */
+public class CentroidTest
+{
+
+    /**
+     * Test method for {@link net.ijt.regfeat.morpho2d.Centroid#populateTable(ij.measure.ResultsTable, java.lang.Object)}.
+     */
+    @Test
+    public final void testPopulateTable()
+    {
+        ImagePlus labelMap = createImagePlus();
+        RegionAnalyisData data = new RegionAnalyisData(labelMap, LabelImages.findAllLabels(labelMap));
+        
+        Centroid feature = new Centroid();
+        Point2D[] res = (Point2D[]) feature.compute(data);
+                
+        Point2D res3 = res[3];
+        assertEquals(res3.getX(), 4.5, 0.01);
+        assertEquals(res3.getY(), 4.5, 0.01);
+    }
+
+    /**
+     * Test method for {@link net.ijt.regfeat.morpho2d.Centroid#compute(net.ijt.regfeat.RegionAnalyisData)}.
+     */
+    @Test
+    public final void testComputeRegionAnalyisData()
+    {
+        ImagePlus labelMap = createImagePlus();
+        RegionAnalyisData data = new RegionAnalyisData(labelMap, LabelImages.findAllLabels(labelMap));
+        Centroid feature = new Centroid();
+
+        ResultsTable table = new ResultsTable();
+        feature.populateTable(table, feature.compute(data));
+        
+        assertEquals(4, table.getCounter());
+        assertEquals(1, table.getLastColumn());
+    }
+    
+    private static final ImagePlus createImagePlus()
+    {
+        ImageProcessor array = new ByteProcessor(8, 8);
+        array.set(1, 1, 3);
+        for (int i = 3; i < 7; i++)
+        {
+            array.set(i, 1, 5);
+            array.set(1, i, 8);
+        }
+        for (int i = 3; i < 7; i++)
+        {
+            for (int j = 3; j < 7; j++)
+            {
+                array.set(i, j, 9);
+            }
+        }
+        return new ImagePlus("labels", array);
+    }
+}
