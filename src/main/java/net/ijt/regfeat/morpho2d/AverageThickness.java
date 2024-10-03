02b9d43a52008cd05c6450032c272580947d77fc
dlegland <david.legland@inrae.fr>
2024-10-03 15:48:23 +0200

    add AverageThickness.java

---
+++ src/main/java/net/ijt/regfeat/morpho2d/AverageThickness.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package net.ijt.regfeat.morpho2d;
+
+import ij.measure.Calibration;
+import ij.measure.ResultsTable;
+import ij.process.ImageProcessor;
+import inra.ijpb.measure.region2d.AverageThickness.Result;
+import net.ijt.regfeat.Feature;
+import net.ijt.regfeat.RegionFeatures;
+
+/**
+ * 
+ */
+public class AverageThickness extends Feature
+{
+
+    @Override
+    public Result[] compute(RegionFeatures data)
+    {
+        // retrieve label map and list of labels
+        ImageProcessor labelMap = data.labelMap.getProcessor();
+        int[] labels = data.labels;
+        Calibration calib = data.labelMap.getCalibration();
+        
+        return new inra.ijpb.measure.region2d.AverageThickness().analyzeRegions(labelMap, labels, calib);
+    }
+
+    @Override
+    public void updateTable(ResultsTable table, RegionFeatures data)
+    {
+        Object obj = data.results.get(this.getClass());
+        if (obj instanceof Result[])
+        {
+            Result[] array = (Result[]) obj;
+            for (int r = 0; r < array.length; r++)
+            {
+                table.setValue("Average_Thickness", r, array[r].avgThickness);
+            }
+        }
+        else
+        {
+            throw new RuntimeException("Requires object argument to be an array of double");
+        }
+    }
+
+}
