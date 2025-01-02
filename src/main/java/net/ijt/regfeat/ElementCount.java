ae4bde8f89439cbcb6ab100e4556ae9355493cbf
dlegland <david.legland@inrae.fr>
2025-01-02 15:58:17 +0100

    add ElementCount feature

---
+++ src/main/java/net/ijt/regfeat/ElementCount.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package net.ijt.regfeat;
+
+import java.util.Map;
+
+import ij.ImageStack;
+import ij.measure.ResultsTable;
+import ij.process.ImageProcessor;
+import inra.ijpb.label.LabelImages;
+
+/**
+ * Counts the number of elements (pixels or voxels) that compose each region.
+ */
+public class ElementCount implements RegionFeature
+{
+    @Override
+    public int[] compute(RegionFeatures data)
+    {
+        // create map of labels to indices
+        Map<Integer, Integer> labelIndices = LabelImages.mapLabelIndices(data.labels);
+        
+        // retrieve image size
+        ImageStack stack = data.labelMap.getStack();
+        int sizeX = stack.getWidth();
+        int sizeY = stack.getHeight();
+        int nSlices = stack.getSize();
+        
+        // allocate memory
+        int[] counts = new int[data.labels.length];
+        
+        // iterate over slices
+        for (int z = 1; z <= nSlices; z++)
+        {
+            ImageProcessor slice = stack.getProcessor(z);
+            
+            // iterate over slice pixels
+            for (int y = 0; y < sizeY; y++)
+            {
+                for (int x = 0; x < sizeX; x++)
+                {
+                    // retrieve current label
+                    int label = (int) slice.getf(x, y);
+                    
+                    // process only labels specified in data
+                    if (label == 0) continue;
+                    if (!labelIndices.containsKey(label)) continue;
+                    
+                    // update result
+                    counts[labelIndices.get(label)]++;
+                }
+            }
+        }
+        
+        return counts;
+    }
+
+    @Override
+    public void updateTable(ResultsTable table, RegionFeatures data)
+    {
+        Object obj = data.results.get(this.getClass());
+        if (obj instanceof int[])
+        {
+            int[] array = (int[]) obj;
+            for (int r = 0; r < array.length; r++)
+            {
+                table.setValue("Count", r, array[r]);
+            }
+        }
+        else
+        {
+            throw new RuntimeException("Requires object argument to be an array of ints");
+        }
+    }
+
+}
