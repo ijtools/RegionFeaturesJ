1790131ae8c3fd99f673c949868de1148eeaaa00
dlegland <david.legland@inrae.fr>
2025-01-06 13:10:16 +0100

    add Centroid3D

---
+++ src/main/java/net/ijt/regfeat/morpho3d/Centroid3D.java
@@ -0,0 +1,109 @@
+/**
+ * 
+ */
+package net.ijt.regfeat.morpho3d;
+
+import java.util.HashMap;
+
+import ij.ImageStack;
+import ij.measure.Calibration;
+import ij.measure.ResultsTable;
+import inra.ijpb.geometry.Point3D;
+import inra.ijpb.label.LabelImages;
+import net.ijt.regfeat.RegionFeature;
+import net.ijt.regfeat.RegionFeatures;
+
+/**
+ * Computes centroid of each regions within the label map.
+ */
+public class Centroid3D implements RegionFeature
+{
+    @Override
+    public Point3D[] compute(RegionFeatures data)
+    {
+        ImageStack image = data.labelMap.getStack();
+        
+        // size of image
+        int sizeX = image.getWidth();
+        int sizeY = image.getHeight();
+        int sizeZ = image.getSize();
+
+        // retrieve spatial calibration
+        double sx = 1, sy = 1, sz = 1;
+        double ox = 0, oy = 0, oz = 0;
+        Calibration calib = data.labelMap.getCalibration();
+        if (calib != null)
+        {
+            sx = calib.pixelWidth;
+            sy = calib.pixelHeight;
+            sz = calib.pixelDepth;
+            ox = calib.xOrigin;
+            oy = calib.yOrigin;
+            oz = calib.zOrigin;
+        }
+        
+        // create associative array to know index of each label
+        HashMap<Integer, Integer> labelIndices = LabelImages.mapLabelIndices(data.labels);
+
+        // allocate memory for result
+        int nLabels = data.labels.length;
+        int[] counts = new int[nLabels];
+        double[] cx = new double[nLabels];
+        double[] cy = new double[nLabels];
+        double[] cz = new double[nLabels];
+
+//        fireStatusChanged(this, "Compute centroids");
+        // compute centroid of each region
+        for (int z = 0; z < sizeZ; z++) 
+        {
+            for (int y = 0; y < sizeY; y++) 
+            {
+                for (int x = 0; x < sizeX; x++)
+                {
+                    int label = (int) image.getVoxel(x, y, z);
+                    if (label == 0)
+                        continue;
+
+                    // do not process labels that are not in the input list 
+                    if (!labelIndices.containsKey(label))
+                        continue;
+
+                    int index = labelIndices.get(label);
+                    cx[index] += x * sx;
+                    cy[index] += y * sy;
+                    cz[index] += z * sz;
+                    counts[index]++;
+                }
+            }
+        }
+        // normalize by number of pixels in each region
+        Point3D[] points = new Point3D[nLabels];
+        for (int i = 0; i < nLabels; i++)
+        {
+            points[i] = new Point3D(cx[i] / counts[i] + ox, cy[i] / counts[i] + oy, cz[i] / counts[i] + oz);
+        }
+
+        return points;
+    }
+    
+    @Override
+    public void updateTable(ResultsTable table, RegionFeatures data)
+    {
+        Object obj = data.results.get(this.getClass());
+        if (obj instanceof Point3D[])
+        {
+            Point3D[] array = (Point3D[]) obj;
+            for (int r = 0; r < array.length; r++)
+            {
+                Point3D point = array[r];
+                table.setValue("Centroid_X", r, point.getX());
+                table.setValue("Centroid_Y", r, point.getY());
+                table.setValue("Centroid_Z", r, point.getZ());
+            }
+        }
+        else
+        {
+            throw new RuntimeException("Requires object argument to be an array of Point3D");
+        }
+    }
+}
---
+++ src/test/java/net/ijt/regfeat/morpho3d/Centroid3DTest.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package net.ijt.regfeat.morpho3d;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ij.ImagePlus;
+import ij.measure.ResultsTable;
+import inra.ijpb.geometry.Point3D;
+import inra.ijpb.label.LabelImages;
+import net.ijt.regfeat.RegionFeatures;
+
+/**
+ * 
+ */
+public class Centroid3DTest
+{
+    /**
+     * Test method for {@link net.ijt.regfeat.morpho3d.Volume#compute(net.ijt.regfeat.RegionFeatures)}.
+     */
+    @Test
+    public final void testCompute()
+    {
+        ImagePlus labelMap = TestImages.createLabeMap_EightRegions_9x9x9();
+        
+        RegionFeatures data = new RegionFeatures(labelMap, LabelImages.findAllLabels(labelMap));
+        
+        Centroid3D feature = new Centroid3D();
+        Point3D[] res = (Point3D[]) feature.compute(data);
+                
+        assertEquals(res.length, 8);
+        Point3D[] exp = new Point3D[] {
+                new Point3D(1, 1, 1),
+                new Point3D(5, 1, 1),
+                new Point3D(1, 5, 1),
+                new Point3D(5, 5, 1),
+                new Point3D(1, 1, 5),
+                new Point3D(5, 1, 5),
+                new Point3D(1, 5, 5),
+                new Point3D(5, 5, 5),
+        };
+        for (int i = 0; i < 8; i++)
+        {
+            Point3D p = res[i];
+            assertEquals(p.getX(), exp[i].getX(), 0.01);
+            assertEquals(p.getY(), exp[i].getY(), 0.01);
+            assertEquals(p.getZ(), exp[i].getZ(), 0.01);
+        }
+    }
+
+    /**
+     * Test method for {@link net.ijt.regfeat.SingleValueFeature#updateTable(ij.measure.ResultsTable, net.ijt.regfeat.RegionFeatures)}.
+     */
+    @Test
+    public final void testUpdateTable()
+    {
+        ImagePlus labelMap = TestImages.createLabeMap_EightRegions_9x9x9();
+        
+        ResultsTable table = RegionFeatures.initialize(labelMap)
+                .add(Centroid3D.class)
+                .createTable();
+        
+        assertEquals(8, table.getCounter());
+        assertEquals(2, table.getLastColumn());
+        
+        assertTrue("Centroid_X".equals(table.getColumnHeading(0)));
+        assertTrue("Centroid_Y".equals(table.getColumnHeading(1)));
+        assertTrue("Centroid_Z".equals(table.getColumnHeading(2)));
+    }
+
+}
